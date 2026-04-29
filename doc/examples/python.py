8aadff71a7475e76b9f9686d2a2e330b7a77baae
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-04-29 06:51:01 +0000

    Built site for gh-pages

--- doc/examples/python.py
+++ doc/examples/python.py
@@ -5,12 +5,12 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.17.2
+#       jupytext_version: 1.19.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
-#     path: /opt/hostedtoolcache/Python/3.10.18/x64/share/jupyter/kernels/python3
+#     path: /opt/hostedtoolcache/Python/3.10.20/x64/share/jupyter/kernels/python3
 # ---
 
 # %% [markdown]
